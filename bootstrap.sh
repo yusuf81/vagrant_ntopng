f96531fe4904251e9a514e7992b74e692cfa5fc3
Sebastien Dupont <dupont.sebastien@gmail.com>
2015-11-18 20:01:06 +0100

    use the apt install described here: http://rsabalburo.blogspot.be/2014/07/ntopng-on-ubuntu-1404-lts-server.html
    note: this is launching ntopng inside a screen, but should use the service instead

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,42 +1,24 @@
 #!/usr/bin/env bash
+# source: http://rsabalburo.blogspot.be/2014/07/ntopng-on-ubuntu-1404-lts-server.html
 
 apt-get update
 apt-get -y upgrade
 
-# Dependencies
-apt-get install -y build-essential libsqlite3-0 libgeoip1 redis-server librrd4 libhiredis0.10 libcurl3 gdb
-apt-get install -y libtool autoconf automake subversion rrdtool wget pkg-config git
-apt-get install -y libsqlite3-dev libhiredis-dev libpcap-dev  libgeoip-dev libcurl4-gnutls-dev libjson-c-dev libglib2.0-dev libxml2-dev libmysqlclient-dev
+sh -c 'echo "deb http://apt.ntop.org/14.04/ x64/\ndeb http://apt.ntop.org/14.04/ all/" > /etc/apt/sources.list.d/ntop.list'
 
-# ndpi
-cd /home/vagrant
-pwd
-git clone https://github.com/ntop/nDPI.git
-cd nDPI
-./autogen.sh
-make
-
-# ntopng
-cd /home/vagrant
-pwd
-git clone https://github.com/ntop/ntopng
-cd ntopng
-pwd
-./autogen.sh
-./configure
-make geoip
-make 
-
-# nBox
-cd /home/vagrant
-pwd
-wget http://apt.ntop.org/14.04/all/apt-ntop.deb
-sudo dpkg -i apt-ntop.deb
-rm -rf apt-ntop.deb
-apt-get clean all
+wget -qO - http://packages.ntop.org/apt/ntop.key | sudo apt-key add -
 apt-get update
-wget http://launchpadlibrarian.net/49822407/snmp-mibs-downloader_1.1_all.deb
-apt-get install smistrip
-dpkg -i snmp-mibs-downloader_1.1_all.deb 
-rm -rf  snmp-mibs-downloader_1.1_all.deb 
-apt-get install -y nbox
+
+apt-get install -y libpcap-dev libglib2.0-dev libgeoip-dev redis-server libxml2-dev libnl1
+apt-get install -y ntopng pfring nprobe ntopng-data n2disk nbox
+
+mkdir /etc/ntopng/
+cp /vagrant/ntopng.conf /etc/ntopng/ntopng.conf
+cp /vagrant/ntopng.start /etc/ntopng/ntopng.start
+
+# really useful?
+sudo service redis-server restart
+sudo service ntopng restart
+
+# launch ntopng in screen
+screen -d -m -S ntopng /usr/local/bin/ntopng --community
